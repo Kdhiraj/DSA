681179e6d4d198c942e4c983aba3ede437a41673
Dhirajk <dhiraj.kumar1@appinventiv.com>
2024-07-06 11:44:44 +0530

    Linear search questions

---
+++ SEARCH_ALGO/src/EvenDigits.java
@@ -0,0 +1,73 @@
+/**
+ * 1295. Find Numbers with Even Number of Digits
+ * Given an array nums of integers, return how many of them contain an even number of digits.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [12,345,2,6,7896]
+ * Output: 2
+ * Explanation:
+ * 12 contains 2 digits (even number of digits).
+ * 345 contains 3 digits (odd number of digits).
+ * 2 contains 1 digit (odd number of digits).
+ * 6 contains 1 digit (odd number of digits).
+ * 7896 contains 4 digits (even number of digits).
+ * Therefore, only 12 and 7896 contain an even number of digits.
+ * Example 2:
+ * <p>
+ * Input: nums = [555,901,482,1771]
+ * Output: 1
+ * Explanation:
+ * Only 1771 contains an even number of digits.
+ */
+
+
+public class EvenDigits {
+
+    public static void main(String[] args) {
+
+        int[] numbs1 = {12, 345, 2, 6, 7896};
+        int[] numbs2 = {555,901,482,1771};
+        int ans = findNumbers(numbs1);
+        int ans2 = findNumbers(numbs2);
+        System.out.println(ans);
+        System.out.println(ans2);
+    }
+
+    static int findNumbers(int[] nums) {
+        int count = 0;
+        for (int num : nums) {
+            if (checkEven(num)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    static boolean checkEven(int num) {
+        int digit = countDigits2(num);
+        return digit % 2 == 0;
+    }
+   //Brute force method
+    static int countDigits(int nums) {
+        if (nums == 0) return 1;
+        if (nums < 0) return nums * -1;
+
+        int count = 0;
+        while (nums > 0) {
+            nums = nums / 10;
+            count++;
+        }
+        return count;
+    }
+
+    //Optimise Way
+
+    static int countDigits2(int nums){
+        if (nums == 0) return 1;
+        if (nums < 0) return nums * -1;
+        return (int) Math.log10(nums) + 1;
+    }
+
+
+}
---
+++ SEARCH_ALGO/src/MaxWealth.java
@@ -0,0 +1,111 @@
+import java.util.ArrayList;
+
+// 1672. Richest Customer Wealth
+/*
+You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank.
+Return the wealth that the richest customer has.
+
+A customer's wealth is the amount of money they have in all their bank accounts. The richest customer is the customer that has the maximum wealth.
+
+
+
+Example 1:
+
+Input: accounts = [[1,2,3],[3,2,1]]
+Output: 6
+Explanation:
+1st customer has wealth = 1 + 2 + 3 = 6
+2nd customer has wealth = 3 + 2 + 1 = 6
+Both customers are considered the richest with a wealth of 6 each, so return 6.
+Example 2:
+
+Input: accounts = [[1,5],[7,3],[3,5]]
+Output: 10
+Explanation:
+1st customer has wealth = 6
+2nd customer has wealth = 10
+3rd customer has wealth = 8
+The 2nd customer is the richest with a wealth of 10.
+Example 3:
+
+Input: accounts = [[2,8,7],[7,1,3],[1,9,5]]
+Output: 17
+
+* */
+public class MaxWealth {
+    public static void main(String[] args) {
+        int[][] numbs1 = {
+                {1, 2, 3},
+                {3, 2, 1}
+        };
+
+        int[][] numbs2 = {
+                {1, 5}, {7, 3}, {3, 5}
+        };
+        int[][] numbs3 = {
+                {2, 8, 7}, {7, 1, 3}, {1, 9, 5}
+        };
+//
+//        System.out.println(maximumWealth(numbs1));
+//        System.out.println(maximumWealth(numbs2));
+//        System.out.println(maximumWealth(numbs3));
+        System.out.println(maxWealth(numbs1));
+        System.out.println(maxWealth(numbs2));
+        System.out.println(maxWealth(numbs3));
+
+    }
+
+    /*
+     * logic:
+     * 1. iterate over each row and do sum of each column element
+     * 2. check greatest
+     * 3. return greatest
+     *
+     */
+
+    static int maximumWealth(int[][] numbs) {
+        ArrayList<Integer> sumList = new ArrayList<>();
+        for (int[] numb : numbs) {
+            int sum = sumOfArray(numb);
+            sumList.add(sum);
+        }
+        return greatestNum(sumList);
+    }
+
+    static int greatestNum(ArrayList<Integer> numbs) {
+        int max = numbs.getFirst();
+        for (int num : numbs) {
+            if (num > max) {
+                max = num;
+            }
+        }
+        return max;
+    }
+
+    static int sumOfArray(int[] numb) {
+        int sum = 0;
+        for (int num : numb) {
+            sum = sum + num;
+        }
+        return sum;
+    }
+
+    //In one method
+    static int maxWealth(int[][] accounts) {
+        //person = row
+        //account = column
+        int max = Integer.MIN_VALUE;
+        for (int customer = 0; customer < accounts.length; customer++) {
+            int sum = 0;
+            for (int account = 0; account < accounts[customer].length; account++) {
+                sum = sum + accounts[customer][account];
+            }
+            if (sum > max) {
+                max = sum;
+            }
+        }
+        return max;
+    }
+
+
+}
