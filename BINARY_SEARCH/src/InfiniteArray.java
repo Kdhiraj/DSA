0e1b2fd64c869072b5361d965f2cd0586a6d881c
Dhirajk <dhiraj.kumar1@appinventiv.com>
2024-07-11 10:05:05 +0530

    refactor the infinite array code

--- BINARY_SEARCH/src/InfiniteArray.java
+++ BINARY_SEARCH/src/InfiniteArray.java
@@ -1,5 +1,18 @@
 //Find position of an element in a sorted array of infinite numbers
+/*
+Suppose you have a sorted array of infinite numbers, how would you search an element in the array?
+Source: Amazon Interview Experience.
+Since array is sorted, the first thing clicks into mind is binary search, but the problem here is that we don’t know size of array.
+If the array is infinite, that means we don’t have proper bounds to apply binary search.
+So in order to find position of key, first we find bounds (start, end index) and then apply binary search algorithm.
+Let low be pointing to 1st element and high pointing to 2nd element of array, Now compare key with high index element,
+->if it is greater than high index element then copy high index in low index and double the high index.
+->if it is smaller, then apply binary search on high and low indices found.
 
+
+Below are implementations of above algorithm
+
+ */
 public class InfiniteArray {
     public static void main(String[] args) {
         int[] arr = {3, 5, 7, 9, 10, 90, 100, 130, 140, 160, 170};
@@ -13,8 +26,8 @@ public class InfiniteArray {
 
         // Double the range until target is within range or end of the array is reached
         while (end < arr.length && arr[end] < target) {
-            start = end ;
-            end = end * 2;
+            start = end ;  // store previous high
+            end = end * 2; // // double high index
             if (end >= arr.length) {
                 end = arr.length - 1;
                 break;
